be854ad657ccc2d7170f64c03fe004c49467c798
Takatoshi Wada <fueledbytaching@gmail.com>
2020-06-17 14:21:18 +0800

    sidebar component and vuertify setting update

--- pages/matter/index.vue
+++ pages/matter/index.vue
@@ -1,118 +1,70 @@
 <template>
   <v-app>
-    <v-navigation-drawer class="sidebar">
-      <div class="sidebar_container">
-        <v-img
-          class="sidebar_container_logo"
-          :src="require('~/static/common/logo.svg')"
-        >
-        </v-img>
-        <h5>株式会社ファーストグループ</h5>
-        <v-select
-          background-color="#F2F6FA"
-          height="35"
-          class="sidebar_container_select"
-          :shops="label"
-          label="cars足立"
-          solo
-        >
-        </v-select>
-        <v-list>
-          <v-list-item-group v-model="model">
-            <v-list-item v-for="(item, i) in items" :key="i">
-              <v-list-item-icon>
-                <v-img
-                  :src="require(`~/static/sidebar/${item.icon}.svg`)"
-                ></v-img>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title v-text="item.text"></v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
+    <Sidebar />
+    <div class="main-container">
+      <div class="breadcrumbs">
+        <!-- <nuxt-link to="/" class="breadcrumbs_home">
+          <v-img
+            class="breadcrumbs_home-logo"
+            :src="require('~/static/breadcrumbs/home.svg')"
+          >
+          </v-img>
+        </nuxt-link> -->
+        <v-breadcrumbs nuxt :items="items">
+          <template v-slot:divider>
+            <v-img :src="require('~/static/breadcrumbs/right.svg')"> </v-img>
+          </template>
+        </v-breadcrumbs>
       </div>
-    </v-navigation-drawer>
-    <v-main>
-      <v-container class="fill-height" fluid>
-        <v-row justify="center" align="center">
-          <v-col class="shrink"> </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
+    </div>
   </v-app>
 </template>
 
 <script>
+import Sidebar from '~/components/Sidebar.vue'
 export default {
-  // props: {
-  //   source: String,
-  // },
-  components: {},
+  components: {
+    Sidebar,
+  },
   data: () => ({
-    drawer: null,
-    left: true,
-    shops: ['Foo', 'Bar', 'Fizz', 'Buzz'],
     items: [
       {
-        icon: 'result',
+        text: 'home',
+        disabled: false,
+        to: '/',
+      },
+      {
+        icon: 'home',
         text: '案件管理',
-      },
-      {
-        icon: 'assessment',
-        text: '査定',
-      },
-      {
-        icon: 'customer',
-        text: '顧客管理',
-      },
-      {
-        icon: 'matching',
-        text: 'マッチング',
-      },
-      {
-        icon: 'setting',
-        text: '管理',
+        disabled: false,
+        to: 'breadcrumbs_link_1',
       },
     ],
-    model: 1,
   }),
 }
 </script>
 <style lang="scss">
-.v-navigation-drawer {
-  width: 210px !important;
+.v-application {
+  background: url('~static/common/full.png') !important;
+  background-size: cover !important;
+  background-repeat: no-repeat !important;
 }
-.v-input__slot {
-  box-shadow: none !important;
-  .v-label {
-    color: $blue-100 !important;
-  }
-}
-.v-list-item__content {
-  color: $blue-100 !important;
-}
-.theme--light.v-select .v-select__selection--comma {
-  color: $blue-100 !important;
+.v-application--wrap {
+  flex-direction: row !important;
 }
 </style>
-<style scoped lang="scss">
-.sidebar {
-  padding-top: 25px;
-  &_container {
-    padding: 0px 13px;
-    &_logo {
-      width: 110px;
-      height: 75px;
-      margin: 0 auto;
-    }
-    h5 {
-      margin-top: 28px;
-      color: $blue-100;
-      margin-bottom: 7px;
-      font-weight: 400;
-    }
-    &_select {
+<style lang="scss" scoped>
+.main-container {
+  padding: 30px 70px 0px;
+  width: 100%;
+  .breadcrumbs {
+    &_home {
+      height: 15px;
+      width: 15px;
+      &-logo {
+        height: 15px;
+        width: 15px;
+      }
     }
   }
 }
