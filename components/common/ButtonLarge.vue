c78e87d62c99fb7a7247da5973a6cac115fe1a59
bunhyoLI <p.li.bunhyo@firstgroup.jp>
2020-06-18 10:21:25 +0900

    added login pages

---
+++ components/common/ButtonLarge.vue
@@ -0,0 +1,62 @@
+<template>
+  <button class="button" :class="[isWhite ? 'white_style' : 'blue_style']">
+    {{ content }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonLarge',
+  props: {
+    content: {
+      type: String,
+      default: '',
+    },
+    isWhite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.button {
+  width: 220px;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 20px;
+  font-weight: bold;
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 15px;
+    right: 20px;
+    width: 10px;
+    height: 10px;
+    transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+  }
+}
+
+.blue_style {
+  background: $blue-100;
+  border: 1px solid $blue-100;
+  color: $white-100;
+  &::after {
+    border-top: 2px solid $white-100;
+    border-right: 2px solid $white-100;
+  }
+}
+
+.white_style {
+  border: 1px solid $blue-100;
+  color: $blue-100;
+  &::after {
+    border-top: 2px solid $blue-100;
+    border-right: 2px solid $blue-100;
+  }
+}
+</style>
---
+++ components/common/Input.vue
@@ -0,0 +1,45 @@
+<template>
+  <input
+    class="input"
+    :type="inputType"
+    :name="name"
+    :placeholder="placeholderText"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  props: {
+    inputType: {
+      type: String,
+      default: 'input',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    placeholderText: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.input {
+  width: 250px;
+  padding: 0 10px;
+  height: 40px;
+  border: 2px solid $gray-100;
+  border-radius: 5px;
+  line-height: 40px;
+  clear: both;
+
+  &::-webkit-input-placeholder {
+    font-size: 14px;
+    color: $blue-100;
+  }
+}
+</style>
