0cbc9c8404ba4606a992ea002a76245eef19e211
Takatoshi Wada <fueledbytaching@gmail.com>
2020-06-30 15:41:04 +0800

    assess result page

---
+++ components/assess/ResultInformation.vue
@@ -0,0 +1,313 @@
+<template>
+  <div class="d-flex flex-column">
+    <div class="tab">
+      <table class="tableStyle table-w50 mb25">
+        <tbody>
+          <tr>
+            <td>入庫予定日</td>
+            <td>2週</td>
+            <td>代替予定車</td>
+            <td>クラウンHV</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 class="title">基本情報</h3>
+      <table class="tableStyle">
+        <tbody>
+          <tr>
+            <td>登録番号</td>
+            <td>足立 310 あ 1234</td>
+            <td>初度登録</td>
+            <td>平成30年1月</td>
+            <td>登録年月日</td>
+            <td>平成30年1月26日</td>
+            <td>車検満了日</td>
+            <td>令和3年1月25日</td>
+          </tr>
+          <tr>
+            <td>メーカー</td>
+            <td>トヨタ</td>
+            <td>型式</td>
+            <td>DAA - ZVW51 - AHXHB</td>
+            <td>車台番号</td>
+            <td>ZVW51 - 0123456</td>
+            <td>排気量</td>
+            <td>1.79 ℓ</td>
+          </tr>
+          <tr>
+            <td>車名</td>
+            <td>プリウス</td>
+            <td>グレード</td>
+            <td>Aプレミアム</td>
+            <td>型式指定番号</td>
+            <td>18029</td>
+            <td>類別区分番号</td>
+            <td>0011</td>
+          </tr>
+          <tr>
+            <td>走行距離</td>
+            <td>22,573 km</td>
+            <td>メーター交換</td>
+            <td>有</td>
+            <td>メーター交換前走行距離</td>
+            <td>15,325 km</td>
+            <td>交換時期</td>
+            <td>令和3年1月25日</td>
+          </tr>
+          <tr>
+            <td>乗員定数</td>
+            <td>5人</td>
+            <td>燃料</td>
+            <td>ハイブリッド</td>
+            <td>駆動</td>
+            <td>FF</td>
+            <td>ドア数</td>
+            <td>5ドア</td>
+          </tr>
+          <tr>
+            <td>スライドドア</td>
+            <td>無</td>
+            <td>Pバックドア</td>
+            <td>有</td>
+            <td>車歴</td>
+            <td>自家用</td>
+            <td>シフト</td>
+            <td>インパネ</td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="tableStyle table-w75 mb25">
+        <tbody>
+          <tr>
+            <td>ミッション</td>
+            <td>CVT</td>
+            <td>最大積載量</td>
+            <td>ー</td>
+            <td>車両重量</td>
+            <td>1,380 kg</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 class="title">所有情報</h3>
+      <table class="tableStyle mb25">
+        <tbody>
+          <tr>
+            <td>所有者</td>
+            <td>米田 道春</td>
+            <td>所有者住所</td>
+            <td>東京都渋谷区渋谷2-11-5</td>
+            <td>使用者</td>
+            <td>所有者に同じ件</td>
+            <td>使用者住所</td>
+            <td>所有者に同じ</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 class="title">輸入車情報</h3>
+      <table class="tableStyle mb25">
+        <tbody>
+          <tr>
+            <td>ハンドル</td>
+            <td>ー</td>
+            <td>VINコード</td>
+            <td>ー</td>
+            <td>正規並行</td>
+            <td>ー</td>
+            <td>登録年月</td>
+            <td>ー</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 class="title">内外装情報</h3>
+      <table class="tableStyle">
+        <tbody>
+          <tr>
+            <td>カラーコード</td>
+            <td>070</td>
+            <td>外板色</td>
+            <td class="w-311">サーモテクトライムグリーン</td>
+            <td>ツートン</td>
+            <td>ルーフ / ブラック</td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="tableStyle table-w75 mb25">
+        <tbody>
+          <tr>
+            <td>トリムコード</td>
+            <td>FA22</td>
+            <td>内装色</td>
+            <td>ブラック</td>
+            <td>革シート</td>
+            <td>無</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 class="title">基本装備情報</h3>
+      <table class="tableStyle">
+        <tbody>
+          <tr>
+            <td>エアバッグ</td>
+            <td>サイド</td>
+            <td>ABS</td>
+            <td>有</td>
+            <td>ホイール</td>
+            <td>純正 / 15 inch</td>
+            <td>ナビ</td>
+            <td>純正 / SD</td>
+          </tr>
+          <tr>
+            <td>TV</td>
+            <td>フルセグ</td>
+            <td>キー</td>
+            <td>スマートキー</td>
+            <td>ETC</td>
+            <td>有</td>
+            <td>サンルーフ</td>
+            <td>有</td>
+          </tr>
+          <tr>
+            <td>バックカメラ</td>
+            <td>有</td>
+            <td>ヘッドライト</td>
+            <td>LED</td>
+            <td>エアコン</td>
+            <td>オート / シングル</td>
+            <td>禁煙車</td>
+            <td>◯</td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="tableStyle table-w75">
+        <tbody>
+          <tr>
+            <td>1オーナー</td>
+            <td>有</td>
+            <td>保証書</td>
+            <td>有</td>
+            <td>取説</td>
+            <td>有</td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="tableStyle table-w50 mb25">
+        <tbody>
+          <tr>
+            <td>整備記録簿</td>
+            <td class="w-311">有 / 平成30年12月26日 / 15,600 km</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 class="title">先進安全装備情報</h3>
+      <table class="tableStyle">
+        <tbody>
+          <tr>
+            <td>運転支援レベル</td>
+            <td>レベルⅡ</td>
+            <td>自動ブレーキ</td>
+            <td>トヨタセーフティセンス</td>
+            <td>ソナー</td>
+            <td>有</td>
+            <td>BSM</td>
+            <td>有</td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="tableStyle table-w50 mb25">
+        <tbody>
+          <tr>
+            <td>レーンキープ</td>
+            <td>ステアリング <br />制御有</td>
+            <td>自動ハイビーム</td>
+            <td>オート</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="vh_center">
+      <ButtonLarge
+        :customwidth="240"
+        content="保存"
+        is-white
+        class="mb30 mr30"
+      />
+      <ButtonLarge
+        :customwidth="240"
+        content="査定情報編集"
+        is-white
+        class="mb30"
+      />
+    </div>
+    <div class="vh_center">
+      <ButtonLarge
+        :customwidth="240"
+        content="削除"
+        class="custombutton mb30"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import ButtonLarge from '~/components/common/ButtonLarge.vue'
+export default {
+  name: 'ResultInformaton',
+  components: {
+    ButtonLarge,
+  },
+}
+</script>
+<style lang="scss" scoped>
+.title {
+  font-size: 20px;
+  color: $blue-200;
+  font-weight: 600;
+  margin-bottom: 25px;
+}
+.tab {
+  width: 850px;
+  height: auto;
+  padding: 30px;
+  border-radius: 5px;
+  margin-bottom: 30px;
+  background: $white-300;
+}
+table.tableStyle {
+  width: 100%;
+  background-color: #ffffff;
+  border-collapse: collapse;
+  border: 1px solid $gray-100;
+  color: $blue-200;
+}
+.table-w50 {
+  width: 50% !important;
+  margin-top: -1px;
+}
+.table-w75 {
+  margin-top: -1px;
+  width: 75% !important;
+}
+.w-311 {
+  width: 311px !important;
+}
+table.tableStyle td {
+  height: 50px;
+  &:nth-child(odd) {
+    width: 77px;
+    font-size: 11px;
+    background-color: $white-100;
+  }
+  &:nth-child(even) {
+    width: 117px;
+    font-size: 12px;
+    text-align: center;
+  }
+}
+table.tableStyle td,
+table.tableStyle th {
+  border: 1px solid $gray-100;
+  padding: 5px;
+}
+.custombutton {
+  border: 1px solid $white-300;
+}
+</style>
