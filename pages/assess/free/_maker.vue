886368a5fb6b80b1ba9f8a42ea04caa66cc5ad3b
rhapsodist <rhapsodist.mk@gmail.com>
2020-07-03 04:39:49 +0900

    use loggedin layout

--- pages/assess/free/_maker.vue
+++ pages/assess/free/_maker.vue
@@ -1,21 +1,17 @@
 <template>
-  <v-app>
-    <Sidebar />
-    <div class="main-container">
-      <Breadcrumbs :breadcrumbs="breadcrumbs" :page="page" />
-      <BodyContainer />
-    </div>
-  </v-app>
+  <div class="main-container">
+    <Breadcrumbs :breadcrumbs="breadcrumbs" :page="page" />
+    <BodyContainer />
+  </div>
 </template>
 
 <script>
-import Sidebar from '~/components/Sidebar.vue'
 import Breadcrumbs from '~/components/Breadcrumbs.vue'
 import BodyContainer from '~/components/assess/free/BodyContainer'
 
 export default {
+  layout: 'loggedin',
   components: {
-    Sidebar,
     Breadcrumbs,
     BodyContainer,
   },
@@ -44,16 +40,6 @@ export default {
   },
 }
 </script>
-<style lang="scss">
-.v-application {
-  background: url('~static/common/full.png') !important;
-  background-size: cover !important;
-  background-repeat: no-repeat !important;
-}
-.v-application--wrap {
-  flex-direction: row !important;
-}
-</style>
 <style lang="scss" scoped>
 .main-container {
   padding: 30px 0px;
