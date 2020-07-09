82409778bd889cedfcc9441a13ea63d7283fcab0
capricorncd <1355952333@qq.com>
2020-07-09 17:36:18 +0900

    Modified breadcrumbs

--- components/Breadcrumbs.vue
+++ components/Breadcrumbs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="breadcrumbs-container">
     <div class="breadcrumbs">
       <v-breadcrumbs :items="breadcrumbs" class="pa-0">
         <template v-slot:item="{ item }">
@@ -30,31 +30,35 @@
         <v-img :src="require('~/static/breadcrumbs/' + personImage)" />
       </v-avatar>
     </div>
-    <div class="titlepage">
-      <div class="titlepage-title">
+    <div class="page-title-wrapper">
+      <div class="left-wrapper">
         <v-img
-          class="titlepage-title__logo mr-2"
+          class="title-icon mr-2"
           :src="require('~/static/breadcrumbs/' + titleImage)"
         />
         <h1 class="d-inline">{{ page }}</h1>
         <h2 class="d-inline">{{ subTitle }}</h2>
       </div>
-      <div v-if="isTitleBtn" class="titlepage-button">
-        <h3>フリー車種入力から作成</h3>
-        <v-img
-          class="titlepage-button__plus mr-2"
-          :src="require('~/static/common/btn-plus.svg')"
-        />
+      <div class="right-wrapper">
+        <slot name="right"></slot>
       </div>
-      <div v-if="isTitlePlus" class="titlepage-plus">
-        <v-img
-          class="titlepage-plus__plus"
-          :src="require('~/static/common/btn-plus.svg')"
-        />
-      </div>
+      <!--      <div v-if="isTitleBtn" class="titlepage-button">-->
+      <!--        <h3>フリー車種入力から作成</h3>-->
+      <!--        <v-img-->
+      <!--          class="titlepage-button__plus mr-2"-->
+      <!--          :src="require('~/static/common/btn-plus.svg')"-->
+      <!--        />-->
+      <!--      </div>-->
+      <!--      <div v-if="isTitlePlus" class="titlepage-plus">-->
+      <!--        <v-img-->
+      <!--          class="titlepage-plus__plus"-->
+      <!--          :src="require('~/static/common/btn-plus.svg')"-->
+      <!--        />-->
+      <!--      </div>-->
     </div>
   </div>
 </template>
+
 <script>
 export default {
   name: 'Breadcrumbs',
@@ -92,82 +96,101 @@ export default {
   },
 }
 </script>
+
 <style lang="scss">
-.v-breadcrumbs__item {
-  color: $white-100 !important;
-}
-</style>
-<style lang="scss" scoped>
-.container {
+.breadcrumbs-container {
   margin-top: 28px;
   padding: 0;
-}
-.breadcrumbs {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 30px;
-  &_home {
-    height: 15px;
-    width: 15px;
-    &-logo {
+
+  .breadcrumbs {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 30px;
+
+    .v-breadcrumbs__item {
+      color: $white-100 !important;
+    }
+
+    &_home {
       height: 15px;
       width: 15px;
+
+      &-logo {
+        height: 15px;
+        width: 15px;
+      }
     }
   }
-}
-.titlepage {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  justify-items: center;
-  margin-bottom: 30px;
-  &-title {
+
+  .page-title-wrapper {
     display: flex;
-    justify-content: flex-start;
-    &__logo {
-      margin-top: 8px;
-      height: 22px;
-      max-width: 22px;
-    }
-    h1 {
-      color: $white-300;
+    align-items: center;
+    justify-content: space-between;
+    justify-items: center;
+    margin-bottom: 30px;
+
+    .left-wrapper {
       display: flex;
-      &__logo {
+      justify-content: flex-start;
+
+      .title-icon {
         margin-top: 8px;
         height: 22px;
         max-width: 22px;
       }
+
+      h1 {
+        color: $white-300;
+        display: flex;
+
+        &__logo {
+          margin-top: 8px;
+          height: 22px;
+          max-width: 22px;
+        }
+      }
+
+      h2 {
+        margin: 5px 0 0 10px;
+        color: $white-300;
+        font-size: 20px;
+      }
     }
-    h2 {
-      margin: 5px 0 0 10px;
-      color: $white-300;
-      font-size: 20px;
+
+    .right-wrapper {
+      display: flex;
+      align-items: center;
     }
-  }
-  &-button {
-    display: flex;
-    background: $white-100;
-    align-items: center;
-    padding: 5px;
-    border-radius: 20px;
-    &__plus {
-      margin-left: 10px;
-      height: 30px;
-      max-width: 30px;
+
+    &-button {
+      display: flex;
+      background: $white-100;
+      align-items: center;
+      padding: 5px;
+      border-radius: 20px;
+
+      &__plus {
+        margin-left: 10px;
+        height: 30px;
+        max-width: 30px;
+      }
+
+      h3 {
+        margin-left: 10px;
+        color: $blue-200;
+      }
     }
-    h3 {
-      margin-left: 10px;
-      color: $blue-200;
-    }
-  }
-  &-plus {
-    background: $white-100;
-    padding: 0;
-    border-radius: 15px;
-    &__plus {
-      height: 30px;
-      max-width: 30px;
+
+    &-plus {
+      background: $white-100;
+      padding: 0;
+      border-radius: 15px;
+
+      &__plus {
+        height: 30px;
+        max-width: 30px;
+      }
     }
   }
 }
---
+++ components/breadcrumbs/PlusButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="plus-button" @click="$emit('click')">
+    <IconPlus />
+  </button>
+</template>
+
+<script>
+import IconPlus from '~/components/breadcrumbs/IconPlus'
+export default {
+  name: 'PlusButton',
+  components: {
+    IconPlus,
+  },
+}
+</script>
+
+<style lang="scss">
+button.plus-button {
+  position: relative;
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  overflow: hidden;
+  background: $white-300;
+  border: 0;
+  outline: none;
+}
+</style>
