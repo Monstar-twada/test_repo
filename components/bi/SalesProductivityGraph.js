2e6522518c573ac0d35967bb79e8846a27eb5182
Takatoshi Wada <fueledbytaching@gmail.com>
2020-07-16 10:58:14 +0800

    gradients for graph

--- components/bi/SalesProductivityGraph.js
+++ components/bi/SalesProductivityGraph.js
@@ -13,6 +13,13 @@ export default {
     },
   },
   mounted() {
+    this.gradientViolet = this.$refs.canvas
+      .getContext('2d')
+      .createLinearGradient(0, 0, 0, 200)
+    this.gradientViolet.addColorStop(0, 'rgb(105, 109, 217)')
+    this.gradientViolet.addColorStop(1, 'rgba(255, 255, 255, 0.5)')
+
+    this.data.datasets[0].backgroundColor = this.gradientViolet
     this.renderChart(this.data, this.options)
   },
 }
