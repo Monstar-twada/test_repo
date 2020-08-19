1e2f0f52ff55497ca0219979cee2236fb6a8576a
Takatoshi Wada <fueledbytaching@gmail.com>
2020-08-19 15:22:40 +0800

    add mailreset page and pin page

---
+++ components/common/login/mailreset/index.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="login-mailreset-page-wrapper">
+    <Logo />
+    <div class="mailreset-form">
+      <h2 class="mailreset-form__title mb5">
+        メールアドレス再設定
+      </h2>
+      <p class="mailreset-form__subtitle mb20">
+        新しいメールアドレスを入力してください。
+      </p>
+      <fg-input
+        v-model="password1"
+        placeholder="新しいメールアドレス"
+        size="medium"
+        class="mb20"
+        suffix-icon="eye"
+        :suffix-icon-color="showPassword ? '#1E5199' : '#DFE6F0'"
+        :type="showPassword ? 'text' : 'password'"
+        @click:suffix-icon="showPassword = !showPassword"
+      />
+      <fg-input
+        v-model="password2"
+        placeholder="新しいメールアドレス（確認）"
+        size="medium"
+        class="mb30"
+        suffix-icon="eye"
+        :suffix-icon-color="showPassword ? '#1E5199' : '#DFE6F0'"
+        :type="showPassword ? 'text' : 'password'"
+        @click:suffix-icon="showPassword = !showPassword"
+      />
+      <fg-button
+        :disabled="matchPassword"
+        class="mailreset-form__button mb20"
+        width="90%"
+        type="primary"
+        suffix-icon="arrow-right"
+        round
+        bold
+        >メールアドレスを設定</fg-button
+      >
+      <fg-button
+        suffix-icon="arrow-right"
+        class="mailreset-form__button"
+        type="primary"
+        border
+        bold
+        width="90%"
+        >戻る</fg-button
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '../Logo.vue'
+export default {
+  components: {
+    Logo,
+  },
+  data() {
+    return {
+      password1: '',
+      password2: '',
+      showPassword: false,
+      matchPassword: true,
+    }
+  },
+  watch: {
+    password1(val) {
+      this.passwordValidation()
+    },
+    password2(val) {
+      this.passwordValidation()
+    },
+  },
+  methods: {
+    passwordValidation() {
+      if (this.password1 === this.password2) {
+        this.matchPassword = false
+      } else {
+        this.matchPassword = true
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.login-mailreset-page-wrapper {
+  padding-top: 160px;
+  .mailreset-form {
+    padding-top: 50px;
+    max-width: 320px;
+    margin: 0 auto;
+    h2,
+    p {
+      text-align: center;
+      color: $blue-200;
+    }
+    &__title {
+      font-weight: 700;
+      font-family: 'Noto Sans JP';
+    }
+    &__button {
+      margin: 0 20px 20px;
+    }
+  }
+}
+</style>
--- components/common/login/pin/index.vue
+++ components/common/login/pin/index.vue
@@ -1,11 +1,117 @@
 <template>
-  <div class="login-pin-page-wrapper">
-    Pin
+  <div class="login-index-page-wrapper">
+    <Logo />
+    <div class="pin-form">
+      <h2>認証コード入力</h2>
+      <h3>ご登録のメールアドレス</h3>
+      <h3>aaaa@cars-enjoy.com</h3>
+    </div>
+    <div class="pin-form__requirements mt25">
+      <p>ご登録のメールアドレス宛に送信した</p>
+      <p>6桁の「認証コード」を入力して送信してください。</p>
+    </div>
+    <div class="pin-form__buttons mt25">
+      <fg-button
+        class="mb20"
+        width="30%"
+        type="primary"
+        suffix-icon="arrow-right"
+        round
+        bold
+        >送信する</fg-button
+      >
+      <fg-button
+        suffix-icon="arrow-right"
+        type="primary"
+        border
+        bold
+        width="30%"
+        >戻る</fg-button
+      >
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Logo from '../Logo.vue'
+export default {
+  components: {
+    Logo,
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+      // eslint-disable-next-line no-useless-escape
+      reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
+      showPassword: false,
+      matchPassword: true,
+    }
+  },
+  watch: {
+    email(val) {
+      this.isValid()
+    },
+    psasword(val) {
+      this.isValid()
+    },
+  },
+  methods: {
+    isEmailValid() {
+      return this.email === ''
+        ? ''
+        : this.reg.test(this.email)
+        ? false
+        : ['login-form__error', true]
+    },
+    isValid() {
+      if (this.isEmailValid() === false && this.password !== '') {
+        return false
+      } else {
+        return true
+      }
+    },
+  },
+}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.login-index-page-wrapper {
+  padding-top: 95px;
+  .pin-form {
+    padding-top: 50px;
+    max-width: 270px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    margin: 0 auto;
+    h2 {
+      color: $blue-200;
+      margin-bottom: 35px;
+    }
+    h3 {
+      color: $blue-200;
+      font-weight: 400;
+    }
+    &__button {
+      margin: 0 20px;
+    }
+    &__error {
+      input[type='text'] {
+        border: 1px solid red;
+      }
+    }
+    &__requirements {
+      text-align: center;
+      color: $blue-200;
+    }
+    &__buttons {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+}
+</style>
