f3114a79d7911f0bf0a8331ac21ea04811ad15b3
Takatoshi Wada <fueledbytaching@gmail.com>
2020-08-20 10:06:29 +0800

    moved logo component to commmon

---
+++ components/common/logo/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-img
+    class="logo"
+    :src="require(`~/components/common/sidebar/img/logo-${logoName}.svg`)"
+    :width="logoWidth"
+  />
+</template>
+<script>
+export default {
+  name: 'Logo',
+  props: {
+    logoWidth: {
+      type: Number,
+      default: 160,
+    },
+  },
+  data() {
+    return {
+      logoName: this.$isManager ? 'manager' : 'dashboard',
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.logo {
+  width: 100%;
+  margin: 0 auto;
+}
+</style>
