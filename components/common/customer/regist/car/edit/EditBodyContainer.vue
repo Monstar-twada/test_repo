bad4bef8921be8e57c07957f1fd5be938be7abe9
Takatoshi Wada <fueledbytaching@gmail.com>
2020-08-21 15:31:02 +0800

    page created

---
+++ components/common/customer/regist/car/edit/EditBodyContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bodyContainer">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditBodyContainer',
+  components: {},
+  data() {},
+}
+</script>
+<style lang="scss" scoped>
+.bodyContainer {
+  width: 100%;
+  /*max-width: 712px;*/
+  margin: 0 auto;
+  color: $blue-200;
+}
+</style>
---
+++ components/common/customer/regist/car/edit/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Breadcrumbs
+      :breadcrumbs="breadcrumbs"
+      :page="page"
+      title-image="result.svg"
+    />
+    <EditBodyContainer />
+  </div>
+</template>
+
+<script>
+import EditBodyContainer from './EditBodyContainer'
+import Breadcrumbs from '~/components/common/breadcrumbs/index'
+export default {
+  components: {
+    Breadcrumbs,
+    EditBodyContainer,
+  },
+  data() {
+    return {
+      page: '顧客管理 [ 車両登録 ]',
+      breadcrumbs: [
+        {
+          text: '顧客管理',
+          href: '/customer',
+        },
+        {
+          text: '車両登録',
+          href: '/customer/car',
+        },
+      ],
+    }
+  },
+}
+</script>
+<style lang="scss"></style>
