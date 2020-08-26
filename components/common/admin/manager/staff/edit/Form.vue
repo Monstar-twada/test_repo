914e25662243526957e86e1a9c18903865b46899
rhapsodist <rhapsodist.mk@gmail.com>
2020-08-27 08:44:06 +0900

    staff form

--- components/common/admin/manager/staff/edit/Form.vue
+++ components/common/admin/manager/staff/edit/Form.vue
@@ -5,50 +5,49 @@
         <h2>基本情報</h2>
       </div>
       <fg-form label-width="120px">
-        <!-- <fg-form-item label="店舗名">
-          <fg-input placeholder="cars足立"></fg-input>
-        </fg-form-item>
-        <fg-form-item label="エリア名">
-          <fg-select
-            v-model="form.areaName"
-            :options="areaItems"
-            width="160px"
-            placeholder="東京エリア"
-            item-prefix="- "
-          />
-        </fg-form-item>
-        <fg-form-item label="郵便番号">
-          <div class="input-small-width">
-            <fg-input placeholder="1200011"></fg-input>
+        <fg-form-item label="権限">
+          <div class="scrollable">
+            asdf
           </div>
         </fg-form-item>
-        <fg-form-item label="都道府県">
+        <fg-form-item label="所属店舗">
           <fg-select
             v-model="form.areaName"
             :options="areaItems"
             width="160px"
-            placeholder="東京都"
+            placeholder="cars板橋"
             item-prefix="- "
           />
         </fg-form-item>
-        <fg-form-item label="市区町村">
-          <fg-input placeholder="足立区中央町"></fg-input>
+        <fg-form-item label="写真">
+          <fg-avatar :src="form.avatarUrl" size="80" fillet></fg-avatar>
         </fg-form-item>
-        <fg-form-item label="番地など">
-          <fg-input placeholder="1-20-1"></fg-input>
+        <fg-form-item label="氏名">
+          <div class="input-small-column">
+            <div class="input-small-column-width">
+              <fg-input placeholder="大倉"></fg-input>
+            </div>
+            <div class="input-small-column-width">
+              <fg-input placeholder="秀樹"></fg-input>
+            </div>
+          </div>
         </fg-form-item>
-        <fg-form-item label="建物名・部屋番号など">
-          <fg-input placeholder=""></fg-input>
+        <fg-form-item label="フリガナ">
+          <div class="input-small-column">
+            <div class="input-small-column-width">
+              <fg-input placeholder="オオクラ"></fg-input>
+            </div>
+            <div class="input-small-column-width">
+              <fg-input placeholder="ヒデキ"></fg-input>
+            </div>
+          </div>
         </fg-form-item>
-        <fg-form-item label="電話番号">
-          <fg-input placeholder="0120-161-135"></fg-input>
+        <fg-form-item label="cars Manager ID">
+          <p class="just-text">df1s082829</p>
         </fg-form-item>
-        <fg-form-item label="メールアドレス">
-          <fg-input placeholder="aaaa@cars-enjoy.com"></fg-input>
+        <fg-form-item label="職種">
+          <p class="just-text">df1s082829</p>
         </fg-form-item>
-        <fg-form-item label="導入サービス">
-          <div class="service-items"></div>
-        </fg-form-item> -->
         <fg-form-item label="権限">
           <fg-radio-group>
             <fg-radio label="スタッフ" value="1"></fg-radio>
@@ -56,12 +55,31 @@
             <fg-radio label="オーナー" value="2"></fg-radio>
           </fg-radio-group>
         </fg-form-item>
+        <fg-form-item label="資格">
+          <fg-input
+            placeholder="1級小型自動車整備士、1級大型自動車整備士、1級二輪自動車整備士"
+          ></fg-input>
+        </fg-form-item>
+        <fg-form-item label="電話番号">
+          <fg-input placeholder="03-3960-7776"></fg-input>
+        </fg-form-item>
+        <fg-form-item label="メールアドレス">
+          <fg-input placeholder="aaaa@cars-enjoy.com"></fg-input>
+        </fg-form-item>
       </fg-form>
     </div>
 
     <div class="admin-staff-edit-form-container_bottom">
       <fg-button suffix-icon="arrow-right" border bold width="220"
         >保存</fg-button
+      >
+      <fg-button
+        suffix-icon="arrow-right"
+        type="primary"
+        border
+        bold
+        width="220"
+        >削除</fg-button
       >
       <fg-button
         suffix-icon="arrow-right"
@@ -83,6 +101,8 @@ export default {
       form: {
         name: '',
         areaName: '',
+        avatarUrl:
+          'https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2700073372,1831939333&fm=26&gp=0.jpg',
       },
     }
   },
@@ -107,13 +127,24 @@ export default {
     .fg-form-item {
       display: grid;
       grid-template-columns: 147px 1fr;
-
-      .input-small-width {
-        width: 160px;
+      align-items: center;
+      .just-text {
+        margin-bottom: 0;
+        font-size: 12px;
+      }
+      .input-small-column {
+        display: flex;
+        align-items: center;
+        &-width {
+          width: 120px;
+          margin-right: 15px;
+        }
       }
       .service-items {
         display: flex;
         align-items: center;
+      }
+      .scrollable {
       }
     }
   }
