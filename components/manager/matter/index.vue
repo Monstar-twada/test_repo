de7b85ec7adc0479cfc11bbf0488e108af90c699
Takatoshi Wada <fueledbytaching@gmail.com>
2020-09-07 16:36:02 +0800

    added search bar

---
+++ components/manager/matter/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Breadcrumbs
+      :breadcrumbs="breadcrumbs"
+      title-image="result.svg"
+      :page="page"
+      task-counter
+    />
+    <SearchConditions />
+  </div>
+</template>
+
+<script>
+import SearchConditions from './index/SearchConditions'
+import Breadcrumbs from '~/components/common/breadcrumbs/index'
+export default {
+  layout: 'manager',
+  // middleware: 'authenticated',
+  components: {
+    Breadcrumbs,
+    SearchConditions,
+  },
+  data: () => ({
+    page: '案件管理',
+    breadcrumbs: [
+      {
+        text: '案件管理',
+        href: '',
+      },
+    ],
+  }),
+}
+</script>
+<style lang="scss" scoped></style>
---
+++ pages/manager/matter/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Customer />
+</template>
+
+<script>
+import Customer from '~/components/manager/matter/index'
+export default {
+  layout: 'manager',
+  components: {
+    Customer,
+  },
+}
+</script>
