85891735b53711789b74d63c5ce8999dd43849a0
rhapsodist <rhapsodist.mk@gmail.com>
2020-09-11 11:06:42 +0900

    merged

--- components/common/admin/manager/company/index/LeftWrapper.vue
+++ components/common/admin/manager/company/index/LeftWrapper.vue
@@ -16,85 +16,47 @@
     </div>
     <div class="company-left-wrapper_body">
       <div class="main-row">
-        <div class="main-row_left">
-          会社名
-        </div>
-        <div class="main-row_right">
-          株式会社XXXXXX
-        </div>
+        <div class="main-row_left">会社名</div>
+        <div class="main-row_right">株式会社XXXXXX</div>
       </div>
       <div class="main-row">
-        <div class="main-row_left">
-          代表者
-        </div>
-        <div class="main-row_right">
-          田中 太郎
-        </div>
+        <div class="main-row_left">代表者</div>
+        <div class="main-row_right">田中 太郎</div>
       </div>
       <div class="main-row">
-        <div class="main-row_left">
-          設立
-        </div>
-        <div class="main-row_right">
-          1960年
-        </div>
+        <div class="main-row_left">設立</div>
+        <div class="main-row_right">1960年</div>
       </div>
       <div class="main-row">
-        <div class="main-row_left">
-          本社所在地
-        </div>
+        <div class="main-row_left">本社所在地</div>
         <div class="main-row_right">
           <p>〒120-0011</p>
           <p>東京都足立区中央本町1-20-1</p>
         </div>
       </div>
       <div class="main-row">
-        <div class="main-row_left">
-          電話番号
-        </div>
-        <div class="main-row_right">
-          000-0000-0000
-        </div>
+        <div class="main-row_left">電話番号</div>
+        <div class="main-row_right">000-0000-0000</div>
       </div>
       <div class="main-row">
-        <div class="main-row_left">
-          メールアドレス
-        </div>
-        <div class="main-row_right">
-          aaaa@cars-enjoy.com
-        </div>
+        <div class="main-row_left">メールアドレス</div>
+        <div class="main-row_right">aaaa@cars-enjoy.com</div>
       </div>
       <div class="main-row">
-        <div class="main-row_left">
-          店舗数
-        </div>
-        <div class="main-row_right">
-          30店
-        </div>
+        <div class="main-row_left">店舗数</div>
+        <div class="main-row_right">30店</div>
       </div>
       <div class="main-row">
-        <div class="main-row_left">
-          社員数
-        </div>
-        <div class="main-row_right">
-          210名
-        </div>
+        <div class="main-row_left">社員数</div>
+        <div class="main-row_right">210名</div>
       </div>
       <div class="main-row">
-        <div class="main-row_left">
-          年度
-        </div>
-        <div class="main-row_right">
-          4月〜3月
-        </div>
+        <div class="main-row_left">年度</div>
+        <div class="main-row_right">4月〜3月</div>
       </div>
       <div class="main-row">
-        <div class="main-row_left">
-          口座情報
-        </div>
-        <div class="main-row_right">
-          〇〇銀行 〇〇支店
-        </div>
+        <div class="main-row_left">口座情報</div>
+        <div class="main-row_right">〇〇銀行 〇〇支店</div>
       </div>
     </div>
   </div>
