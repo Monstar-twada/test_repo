445c5ccafe68e58619396ba52154bb495916150b
capricorncd <1355952333@qq.com>
2020-09-16 10:32:09 +0900

    fix: object reference error

--- plugins/global-console-properties.js
+++ plugins/global-console-properties.js
@@ -5,7 +5,6 @@ import Vue from 'vue'
 import FirstGroupUI from './first-group-ui/src/index'
 import { RequestApi } from './api/index'
 import { login } from '~/components/console/plugins/login'
-import * as app from '~/assets/app/index'
 
 Vue.use(FirstGroupUI, {
   theme: 'red',
@@ -17,7 +16,7 @@ const extendsProps = {
     // System environment differentiation
     Vue.prototype.$isConsole = true
     // $api
-    Vue.prototype.$api = new RequestApi(['X-CARS-MANAGER'], app)
+    Vue.prototype.$api = new RequestApi(['X-CARS-MANAGER'])
     // $login
     Vue.prototype.$login = login
   },
