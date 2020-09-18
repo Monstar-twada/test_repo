c6dc7cb8e0fca34457adb5978464990335a45262
capricorncd <1355952333@qq.com>
2020-09-18 12:14:46 +0900

    fix: Scroll bar display bug

--- plugins/first-group-ui/src/packages/avatar/src/index.vue
+++ plugins/first-group-ui/src/packages/avatar/src/index.vue
@@ -163,15 +163,15 @@ export default {
     display: inline-flex;
     font-weight: 800;
     margin-left: 10px;
-    line-height: 1;
     align-items: center;
-    overflow-x: hidden;
+    overflow: hidden;
     .__name,
     .__summary {
       white-space: nowrap;
-      overflow-x: hidden;
+      overflow: hidden;
       text-overflow: ellipsis;
       max-width: 100%;
+      line-height: 1;
     }
     .__name {
       font-size: $--font-size-14;
