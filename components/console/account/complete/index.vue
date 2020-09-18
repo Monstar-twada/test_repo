1be7979a189cea3e83ed4a8a0b7f719fc3297fe7
rhapsodist <rhapsodist.mk@gmail.com>
2020-09-18 11:28:13 +0900

    passwordreset && signin && mailreset && account

---
+++ components/console/account/complete/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="console-account-complete-page-wrapper">
+    <Logo />
+    <div class="complete-form">
+      <h2 class="complete-form__title mb5">アカウントの登録が完了しました</h2>
+      <p class="complete-form__subtitle mb40">ログインしてください。</p>
+      <fg-button
+        class="complete-form__button"
+        width="220px"
+        type="primary"
+        suffix-icon="arrow-right"
+        round
+        bold
+        @click="nextUrl('/login')"
+        >ログイン</fg-button
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/common/logo/index'
+export default {
+  components: {
+    Logo,
+  },
+  methods: {
+    nextUrl(url) {
+      this.$router.push({ path: url })
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.console-account-complete-page-wrapper {
+  padding-top: 250px;
+  .complete-form {
+    padding-top: 50px;
+    max-width: 350px;
+    margin: 0 auto;
+    h2,
+    p {
+      text-align: center;
+      color: $--color-primary;
+    }
+    &__title {
+      font-weight: 700;
+      font-family: 'Noto Sans JP';
+    }
+    &__subtitle {
+      font-size: 14px;
+      font-weight: 400;
+    }
+    &__button {
+      margin: 0 60px;
+    }
+  }
+}
+</style>
---
+++ pages/console/account/edit.vue
@@ -0,0 +1,14 @@
+<template>
+  <Edit />
+</template>
+
+<script>
+import Edit from '~/components/console/account/edit/index'
+
+export default {
+  layout: 'login',
+  components: {
+    Edit,
+  },
+}
+</script>
