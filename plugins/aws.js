dfd76ae4321dda3659b53dec6b6195aa7c2cd184
Takatoshi Wada <fueledbytaching@gmail.com>
2020-10-16 15:28:54 +0800

    added env for production

--- plugins/aws.js
+++ plugins/aws.js
@@ -3,15 +3,9 @@ import Auth from '@aws-amplify/auth'
 const plugin = () => {
   Auth.configure({
     Auth: {
-      // プロトタイプ
-      // region: 'us-west-2',
-      // userPoolId: 'us-west-2_9p8NZfOa5',
-      // userPoolWebClientId: '34j525o2171mi0bf7tfuv5ucak',
-
-      // テスト環境
-      region: 'ap-northeast-1',
-      userPoolId: 'ap-northeast-1_WYQfDG450',
-      userPoolWebClientId: '6hqcphuul6im51a9nh3jmuhtu1',
+      region: process.env.COGNITO_REGION,
+      userPoolId: process.env.COGNITO_USERPOOL_ID,
+      userPoolWebClientId: process.env.COGNITO_USERPOOL_CLIENT_ID,
     },
   })
 }
