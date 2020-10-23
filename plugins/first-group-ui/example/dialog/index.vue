8233d1df5a4e4d439429359a57e8d4d70131a6bd
rhapsodist <rhapsodist.mk@gmail.com>
2020-10-23 17:07:16 +0900

    merge develop

--- plugins/first-group-ui/example/dialog/index.vue
+++ plugins/first-group-ui/example/dialog/index.vue
@@ -1,110 +1,5 @@
 <template>
   <div class="example-dialog-wrapper">
-<<<<<<< HEAD
-    <Doc />
-    <h2>default</h2>
-    <div>
-      <fg-button type="primary" width="320px" @click="visible1 = true"
-        >Click</fg-button
-      >
-
-      <fg-button type="primary" width="320px" @click="visible5 = true"
-        >Click</fg-button
-      >
-    </div>
-
-    <h2>persistent</h2>
-    <div>
-      <fg-button type="primary" width="320px" @click="visible2 = true"
-        >Click</fg-button
-      >
-    </div>
-
-    <h2>max-width="640px"</h2>
-    <div>
-      <fg-button type="primary" width="320px" @click="visible3 = true"
-        >Click</fg-button
-      >
-    </div>
-
-    <h2>hide-header-border</h2>
-    <div>
-      <fg-button type="primary" width="320px" @click="visible4 = true"
-        >Click</fg-button
-      >
-    </div>
-
-    <h2>v-slot:title-suffix, v-slot:header-right</h2>
-    <div>
-      <fg-button type="primary" width="320px" @click="visible6 = true"
-        >Click</fg-button
-      >
-    </div>
-
-    <h2>v-slot:header</h2>
-    <div>
-      <fg-button type="primary" width="320px" @click="visible7 = true"
-        >Click</fg-button
-      >
-    </div>
-
-    <fg-dialog v-model="visible1" title="Hello World日本語テスト">
-      content
-    </fg-dialog>
-
-    <fg-dialog v-model="visible2" title="Hello World日本語テスト" persistent>
-      content
-      <div style="padding: 24px">
-        <fg-calendar />
-      </div>
-      <div style="padding: 24px">
-        <fg-calendar />
-      </div>
-    </fg-dialog>
-
-    <!-- max-width="640px"-->
-    <fg-dialog
-      v-model="visible3"
-      title="Hello World日本語テスト"
-      max-width="640px"
-    >
-      content
-    </fg-dialog>
-
-    <!-- hide-header-border-->
-    <fg-dialog v-model="visible4" title="hide-header-border" hide-header-border>
-      hide-header-border
-    </fg-dialog>
-
-    <!-- larger -->
-    <fg-dialog v-model="visible5" title="larger">
-      <div style="overflow: hidden">
-        <p v-for="item in 100" :key="item">text {{ item }}</p>
-      </div>
-    </fg-dialog>
-
-    <fg-dialog
-      v-model="visible6"
-      title="v-slot:title-suffix, v-slot:header-right"
-    >
-      <template v-slot:title-suffix>
-        <fg-icon name="flag" color="red"></fg-icon>
-      </template>
-      <template v-slot:header-right>
-        <fg-icon name="flag" color="red"></fg-icon>
-      </template>
-      content
-    </fg-dialog>
-
-    <fg-dialog v-model="visible7">
-      <template v-slot:header>
-        <fg-icon name="flag" color="green"></fg-icon>
-      </template>
-      <fg-calendar />
-    </fg-dialog>
-
-    <div style="height: 1000px"></div>
-=======
     <Docs />
     <h2>Example</h2>
     <Normal />
@@ -112,7 +7,6 @@
     <MaxWidth />
     <HideHeaderBorder />
     <SlotDemo />
->>>>>>> develop
   </div>
 </template>
 
--- plugins/first-group-ui/example/message-box/index.vue
+++ plugins/first-group-ui/example/message-box/index.vue
@@ -1,47 +1,10 @@
 <template>
-<<<<<<< HEAD
-  <fg-form label-position="top">
-    <fg-form-item label="alert">
-      <fg-button type="primary" width="100%" @click="handleMessageBox('alert')"
-        >Alert</fg-button
-      >
-    </fg-form-item>
-    <fg-form-item label="confirm">
-      <fg-button
-        type="primary"
-        width="100%"
-        @click="handleMessageBox('confirm')"
-        >Confirm</fg-button
-      >
-    </fg-form-item>
-    <fg-form-item
-      label="confirm buttons:{ ok: {text: 'ログアウトする', type: 'waining'} }"
-    >
-      <fg-button
-        type="primary"
-        width="100%"
-        @click="handleMessageBox('warning')"
-        >Confirm</fg-button
-      >
-    </fg-form-item>
-    <fg-form-item label="alert message:[message-p1, message-p2]">
-      <fg-button
-        type="primary"
-        width="100%"
-        @click="handleMessageBox('multiple')"
-        >alert</fg-button
-      >
-      <div style="height: 1000px"></div>
-    </fg-form-item>
-  </fg-form>
-=======
   <div>
     <Docs />
     <h2>Example</h2>
     <Normal />
     <Multiple />
   </div>
->>>>>>> develop
 </template>
 
 <script>
