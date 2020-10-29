5c6a839f97f379d8b6cc4c81530974bcb538db15
Takatoshi Wada <fueledbytaching@gmail.com>
2020-10-29 10:02:52 +0800

    installed vue-pdf

--- components/common/customer/detail/car-info/vehicle-inspection-cert-dialog/index.vue
+++ components/common/customer/detail/car-info/vehicle-inspection-cert-dialog/index.vue
@@ -7,13 +7,21 @@
     title="車検証"
   >
     <div class="body-wrapper">
-      <img :src="url" alt="" />
+      <pdf :src="url" />
+      <!-- <pdf
+        src="https://cdn.rawgit.com/mozilla/pdf.js/c6e8ca86/test/pdfs/freeculture.pdf"
+      /> -->
+      <!-- <img :src="url" alt="" /> -->
     </div>
   </fg-dialog>
 </template>
 
 <script>
+import pdf from 'vue-pdf'
 export default {
+  components: {
+    pdf,
+  },
   props: {
     value: Boolean,
     customerCode: {
@@ -44,6 +52,7 @@ export default {
   },
   mounted() {
     this.getImage()
+    console.log(this.url)
   },
   methods: {
     async getImage() {
@@ -53,6 +62,7 @@ export default {
           `/v1/customers/${this.customerCode}/cars/${this.carCode}/registrationImage`
         )
         this.url = res.url
+        this.url = encodeURI(this.url)
       } catch (err) {
         this.$ui.error('[vehicle-inspection-cert-dialog]', err)
       }
